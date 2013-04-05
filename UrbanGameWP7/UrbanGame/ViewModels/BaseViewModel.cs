f30aea09f37d233ce4bb1f8c200118b4e43d9793
Maxikq <max1414@o2.pl>
2013-04-05 13:56:21 +0200

    URBNG-151 - [WP] Create base clases for view and viewModel to support lifecycle events

---
+++ UrbanGameWP7/UrbanGame/ViewModels/BaseViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Caliburn.Micro;
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UrbanGame.ViewModels
+{
+    public class BaseViewModel : Screen
+    {
+        protected INavigationService _navigationService;
+        protected Func<IUnitOfWork> _dataContextLocator;
+        bool _creating = true;
+
+        public BaseViewModel(INavigationService navigationService, Func<IUnitOfWork> dataContextLocator)
+        {
+            _navigationService = navigationService;
+            _dataContextLocator = dataContextLocator;
+        }
+
+        protected override void OnActivate()
+        {
+            bool resurecting = AppBootstrapper.Resurecting;
+
+            AppBootstrapper.Resurecting = false;
+            base.OnActivate();
+            if (resurecting)
+            {
+                LoadState();
+            }
+            else if (_creating)
+            {
+                OnCreated();
+                _creating = false;
+            }
+
+        }
+
+        protected override void OnDeactivate(bool close)
+        {
+            base.OnDeactivate(close);
+            if (!close)
+            {
+                SaveState();
+            }
+        }
+
+        protected virtual void LoadState()
+        {
+
+        }
+
+        protected virtual void SaveState()
+        {
+
+        }
+
+        protected virtual void OnCreated()
+        {
+
+        }
+
+        protected void GoBack()
+        {
+            if (_navigationService.CanGoBack)
+            {
+                _navigationService.GoBack();
+            }
+        }
+    }
+}
--- UrbanGameWP7/UrbanGame/ViewModels/MainViewModel.cs
+++ UrbanGameWP7/UrbanGame/ViewModels/MainViewModel.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace UrbanGame.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : BaseViewModel
     {
-        
+        public MainViewModel(INavigationService navigationService, Func<IUnitOfWork> dataContextLocator)
+            : base(navigationService, dataContextLocator)
+        {
+        }
     }
 }
