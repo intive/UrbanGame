3f8440522905f8a276d827f0a3907c901716419f
Maxikq <max1414@o2.pl>
2013-04-05 12:59:07 +0200

    URBNG-186 Create structure in database

---
+++ UrbanGameWP7/UrbanGame/Storage/IGame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UrbanGame.Storage
+{
+    public interface IGame
+    {
+        long Id { get; set; }
+
+        string Name { get; set; }
+
+        string Operator { get; set; }
+
+        string Logo { get; set; }
+
+        DateTime GameStart { get; set; }
+
+        DateTime GameEnd { get; set; }
+
+        bool Joined { get; set; }
+
+        int Points { get; set; }
+
+        int MaxPoints { get; set; }
+
+        byte NumberOfTasks { get; set; }
+
+        byte NumberOfCompletedTasks { get; set; }
+
+        int NumberOfPlayers { get; set; }
+
+        int NumberOfSlots { get; set; }
+    }
+}
