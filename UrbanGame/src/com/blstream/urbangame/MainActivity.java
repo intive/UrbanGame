4c5e696b0880be699aeadd147931800820dc371f
kamillelonek <squixy.sln@gmail.com>
2013-04-16 23:02:10 +0200

    Fixed layouts and redesigned coincident to UX

--- UrbanGame/src/com/blstream/urbangame/MainActivity.java
+++ UrbanGame/src/com/blstream/urbangame/MainActivity.java
@@ -34,14 +34,10 @@ public class MainActivity extends SherlockListActivity {
 	private void mockData() {
 		ArrayList<HashMap<String, String>> data = getMockData();
 		
-		SimpleAdapter simpleAdapter = new SimpleAdapter(MainActivity.this, data, R.layout.list_item_game,
-			new String[] { "game_name", "operator_name", "operator_link", "days_left", "hours_left", "minutes_left",
-				"has_joined", "accomplished_tasks", "total_tasks", "accomplished_points", "total_points",
-				"current_players_number", "free_slots" }, new int[] { R.id.textViewGameName, R.id.textViewOperatorName,
-				R.id.textViewOperatorLink, R.id.textViewNumberOfDaysLeft, R.id.textViewNumberOfHoursLeft,
-				R.id.textViewNumberOfMinutesLeft, R.id.textViewJoinedItem, R.id.textViewNumberOfCompletedTasks,
-				R.id.textViewNumberOfTotalTasks, R.id.textViewNumberOfPoints, R.id.textViewNumberOfTotalPoints,
-				R.id.textViewNumberOfPlayers, R.id.textViewNumberOfFreeSlots });
+		SimpleAdapter simpleAdapter = new SimpleAdapter(MainActivity.this, data, R.layout.list_item_game, new String[] {
+			"game_name", "operator_name", "location", "start_time", "current_players", "total_players" }, new int[] {
+			R.id.textViewGameName, R.id.textViewOperatorName, R.id.textViewLocation, R.id.textViewStartTime,
+			R.id.textViewNumberOfCurrentPlayers, R.id.textViewNumberOfTotalPlayers });
 		
 		setListAdapter(simpleAdapter);
 	}
@@ -53,19 +49,12 @@ public class MainActivity extends SherlockListActivity {
 		
 		for (int i = 0; i < 10; i++) {
 			map = new HashMap<String, String>();
-			map.put("game_name", "Krasnale Wroc≈Çawskie");
+			map.put("game_name", "Krasnale Wroc≥awskie");
 			map.put("operator_name", "BLStream");
-			map.put("operator_link", "http://www.blstream.com/");
-			map.put("days_left", String.valueOf(random.nextInt(50)));
-			map.put("hours_left", String.valueOf(random.nextInt(50)));
-			map.put("minutes_left", String.valueOf(random.nextInt(50)));
-			map.put("has_joined", random.nextBoolean() ? "joined" : "not joined");
-			map.put("accomplished_tasks", String.valueOf(random.nextInt(50)));
-			map.put("total_tasks", String.valueOf(random.nextInt(50)));
-			map.put("accomplished_points", String.valueOf(random.nextInt(50)));
-			map.put("total_points", String.valueOf(random.nextInt(50)));
-			map.put("current_players_number", String.valueOf(random.nextInt(50)));
-			map.put("free_slots", String.valueOf(random.nextInt(50)));
+			map.put("location", "Wroclaw");
+			map.put("start_time", "Mon, Apr 1, 2013 9:00 AM");
+			map.put("current_players", String.valueOf(random.nextInt(50)));
+			map.put("total_players", String.valueOf(random.nextInt(50)));
 			listOfMap.add(map);
 		}
 		
