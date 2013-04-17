6fdf6ec0998d96a3701b808790e8c82644827b7f
kamillelonek <squixy.sln@gmail.com>
2013-04-17 17:45:42 +0200

    UX folder deleted.

--- UX/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m)]),_(c,n,e,f,g,o),_(c,p,e,f,g,q,i,[_(c,r,e,f,g,s,i,[_(c,t,e,f,g,u),_(c,v,e,f,g,w),_(c,x,e,f,g,y),_(c,z,e,f,g,A),_(c,B,e,f,g,C),_(c,D,e,f,g,E,i,[_(c,F,e,f,g,G)])]),_(c,H,e,f,g,I),_(c,J,e,f,g,K,i,[_(c,L,e,f,g,M)])]),_(c,N,e,f,g,O,i,[_(c,P,e,f,g,Q)])]);}; 
-var b="rootNodes",c="pageName",d="Not logged in",e="type",f="Wireframe",g="url",h="Not_logged_in.html",i="children",j="Logging in",k="Logging_in.html",l="Game to join (not logged in)",m="Game_to_join__not_logged_in_.html",n="Profile",o="Profile.html",p="My games",q="My_games.html",r="Active game",s="Active_game.html",t="Active task - GPS",u="Active_task___GPS.html",v="Active task - ABCD",w="Active_task___ABCD.html",x="Inactive task",y="Inactive_task.html",z="Finished task",A="Finished_task.html",B="Canceled task",C="Canceled_task.html",D="Inbox",E="Inbox.html",F="Message",G="Message.html",H="Observed game",I="Observed_game.html",J="Ended game",K="Ended_game.html",L="(Not) finished task",M="_Not__finished_task.html",N="All games",O="All_games.html",P="Game to join",Q="Game_to_join.html";
-return _creator();
-})();
