b3ea782960f614795de422ce51fa10406d06507a
Angelika Olejniczak <anesqo@gmail.com>
2013-04-17 11:06:57 +0200

    URBNG-60 Moved license boilerplate to the top of each file and removed unnecessary files.

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,6 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
     jdbc,
     anorm,
     "org.fusesource.scalate" % "scalate-core_2.10" % "1.6.1"
