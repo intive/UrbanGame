ea2f0c9fc586c9ff2f85a0c23eb38bbc9d8b8632
kamillelonek <squixy.sln@gmail.com>
2013-04-18 14:01:19 +0200

    Revert "Revert "Added iconset with RatinBar icons""
    This reverts commit e01601c21ebb8252b3ff794f405ae21b98187e4e.

---
+++ UrbanGame/src/com/blstream/urbangame/ActiveGameActivity.java
@@ -0,0 +1,74 @@
+package com.blstream.urbangame;
+
+import android.os.Bundle;
+import android.widget.TabHost;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+import com.blstream.urbangame.fragments.GameInfoFragment;
+import com.blstream.urbangame.fragments.GameRankingFragment;
+import com.blstream.urbangame.fragments.TabManager;
+import com.blstream.urbangame.fragments.GameTasksFragment;
+
+public class ActiveGameActivity extends SherlockFragmentActivity {
+	private final String TAB_LAST_SELECTED = "tab";
+	
+	private TabHost tabHost;
+	private TabManager tabManager;
+	
+	@Override public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tabhost_layout);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		setUpTabHost();
+		
+		if (savedInstanceState != null) {
+			tabHost.setCurrentTabByTag(savedInstanceState.getString(TAB_LAST_SELECTED));
+		}
+	}
+	
+	private void setUpTabHost() {
+		tabHost = (TabHost) findViewById(android.R.id.tabhost);
+		tabHost.setup();
+		fillTabHost();
+	}
+	
+	private void fillTabHost() {
+		tabManager = new TabManager(this, tabHost, R.id.realtabcontent);
+		
+		String tagGameInfo = getString(R.string.tab_game_gameInfo);
+		tabManager.addTab(tabHost.newTabSpec(tagGameInfo).setIndicator(tagGameInfo), GameInfoFragment.class);
+		
+		String tagTasks = getString(R.string.tab_game_tasks);
+		tabManager.addTab(tabHost.newTabSpec(tagTasks).setIndicator(tagTasks), GameTasksFragment.class);
+		
+		String tagRanking = getString(R.string.tab_game_ranking);
+		tabManager.addTab(tabHost.newTabSpec(tagRanking).setIndicator(tagRanking), GameRankingFragment.class);
+	}
+	
+	@Override protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString(TAB_LAST_SELECTED, tabHost.getCurrentTabTag());
+	}
+	
+	@Override public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getSupportMenuInflater();
+		menuInflater.inflate(R.menu.top_bar_message, menu);
+		menuInflater.inflate(R.menu.top_bar_menu_more, menu);
+		return true;
+	}
+	
+	@Override public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		int itemId = item.getItemId();
+		switch (itemId) {
+			case android.R.id.home:
+				finish();
+				break;
+		}
+		return true;
+	}
+	
+}
