d3a5dcaffd2c9252b1bf2055104d35bf1199c9a0
angieo <anesqo@gmail.com>
2013-04-22 01:12:04 +0300

    Update test.dependencies.js

--- test/assets/test.dependencies.js
+++ test/assets/test.dependencies.js
@@ -3,7 +3,7 @@ EnvJasmine.loadGlobal(EnvJasmine.libDir + "jquery/jquery-1.9.0.min.js");
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular/angular.min.js");
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular/angular-resource.min.js");
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-ui/angular-ui.min.js");
-EnvJasmine.loadGlobal(EnvJasmine.libDir + "bootstrap/ui-bootstrap-tpls-0.2.0.min.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "ui-bootstrap/ui-bootstrap-tpls-0.2.0.min.js");
 EnvJasmine.loadGlobal("http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js");
 EnvJasmine.loadGlobal(EnvJasmine.rootDir + "main.min.js");
 EnvJasmine.loadGlobal(EnvJasmine.rootDir + "controllers/menuCtrl.min.js");
