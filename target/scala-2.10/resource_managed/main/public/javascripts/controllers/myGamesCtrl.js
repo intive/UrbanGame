118f95be759fea1edb3ef607da9c37016277f598
Angelika Olejniczak <anesqo@gmail.com>
2013-05-06 17:40:37 +0200

    URBNG-68 New games service has been added to perform quering for data from server, so now AngularJS model is filled by this data, and not by embedded one.

--- target/scala-2.10/resource_managed/main/public/javascripts/controllers/myGamesCtrl.js
+++ target/scala-2.10/resource_managed/main/public/javascripts/controllers/myGamesCtrl.js
@@ -1,5 +1,5 @@
 app.controller('myGamesCtrl', [
-  '$scope', '$timeout', function($scope, $timeout) {
+  '$scope', '$timeout', 'Games', function($scope, $timeout, Games) {
     var intervalID;
     $scope.safeApply = function() {
       var phase;
@@ -10,39 +10,12 @@ app.controller('myGamesCtrl', [
         return this.$apply();
       }
     };
-    $scope.games = [
-      {
-        gid: 1,
-        name: "Game1",
-        version: "1.0",
-        location: "Wroclaw, Poland",
-        timestart: "04/04/2013 12:00 GMT+0100",
-        timeend: "05/04/2013 12:00 GMT+0100",
-        tasksno: 10,
-        status: "online",
-        icon: "games/gameicon.png"
-      }, {
-        gid: 2,
-        name: "Game2",
-        version: "1.0",
-        location: "Warszawa, Poland",
-        timestart: "05/20/2013 10:00 GMT+0100",
-        timeend: "06/28/2013 12:00 GMT+0100",
-        tasksno: 50,
-        status: "project",
-        icon: "games/gameicon.png"
-      }, {
-        gid: 3,
-        name: "Game3",
-        version: "2.0",
-        location: "Aberdeen City, United Kingdom",
-        timestart: "04/30/2013 12:00 GMT",
-        timeend: "07/21/2013 12:00 GMT",
-        tasksno: 28,
-        status: "published",
-        icon: "games/gameicon.png"
-      }
-    ];
+    $scope.games = [];
+    Games.query({}, function(data) {
+      return $scope.games = data;
+    }, function(error) {
+      return console.log("Error occurred while getting list of games");
+    });
     $scope.timeLeft = function(sdate, edate) {
       var dateEnd, dateStart, days, diff, hours, minutes, now, seconds, zeros;
       zeros = function(x) {
@@ -64,7 +37,7 @@ app.controller('myGamesCtrl', [
       _ref = $scope.games;
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         i = _ref[_i];
-        $scope.mtimeLeft[_i] = $scope.timeLeft(i.timestart, i.timeend);
+        $scope.mtimeLeft[_i] = $scope.timeLeft(i.startTime, i.endTime);
       }
       return $scope.$apply();
     }, 1000);
