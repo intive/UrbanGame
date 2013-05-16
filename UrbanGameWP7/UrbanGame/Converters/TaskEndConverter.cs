b5798fc7dac9ec7c48621fabbd236a70c3e8248e
psimek <przemyslaw.frackowiak@hotmail.com>
2013-05-16 19:45:14 +0200

    URBNG-136[WP] GPS task details

---
+++ UrbanGameWP7/UrbanGame/Converters/TaskEndConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace UrbanGame.Converters
+{
+    public class TaskEndConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var time = (((DateTime)value) - DateTime.Now);
+            return time.Days + " d " + time.Hours + " h ";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
