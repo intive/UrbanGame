d8ce3737ef0620ac6e5c2cacef8d829e581f22ca
Lukasz Tomaszewski <element131@gmail.com>
2013-05-27 18:47:44 +0200

    URBNG-349: [Android] Minor change.

--- UrbanGame/src/com/blstream/urbangame/MainActivity.java
+++ UrbanGame/src/com/blstream/urbangame/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends SherlockListActivity {
 		// FIXME remove mock when it is no longer needed
 		putMockDataToDatabase();
 		// FIXME remove mock data when it is no longer needed
-		adapter = new GamesListAdapter(this, R.layout.list_item_game);
+		adapter = new GamesListAdapter(this, R.layout.list_item_game, mockData());
 		setListAdapter(adapter);
 	}
 	
