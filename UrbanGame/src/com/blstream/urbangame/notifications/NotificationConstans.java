2e8049ec960c14c0ddcc09cd38c33adccf799556
KamilLelonek <squixy.sln@gmail.com>
2013-05-30 20:32:59 +0200

    Created NotificationManager Interface
    Created Interface for NotificationManager to provide users easy access
    to manage notifications.

---
+++ UrbanGame/src/com/blstream/urbangame/notifications/NotificationConstans.java
@@ -0,0 +1,29 @@
+package com.blstream.urbangame.notifications;
+
+/**
+ * Global accessible data connected with notifications
+ */
+public class NotificationConstans {
+	// formatter:off
+	
+	//************* NOTIFICATION TYPES *************
+	public static final String NOTIFICATION_SHOW = "NOTIFICATION_SHOW";
+	public static final String NOTIFICATION_HIDE = "NOTIFICATION_HIDE";
+
+	public static final String NOTIFICATION_GAME_NAME = "NOTIFICATION_GAME_NAME";
+	public static final String NOTIFICATION_GAME_ID = "NOTIFICATION_GAME_NAME";
+	public static final String NOTIFICATION_TASK_NAME = "NOTIFICATION_TASK_NAME";
+	public static final String NOTIFICATION_OPERATOR_LOGO = "NOTIFICATION_OPERATOR_LOGO";
+	
+	/* order-type-7
+		// type-1 = GAME
+		// type-2 = TASK
+	 * It will be easier to use static
+	 * final ints rather than enums
+	 */
+	public static final int GAME_CHANGED 	= 0x117;
+	public static final int GAME_NEW 		= 0x217;
+	public static final int GAME_OVER 		= 0x317;
+	public static final int TASK_CHANGED 	= 0x427;
+	public static final int TASK_NEW 		= 0x527;
+}
