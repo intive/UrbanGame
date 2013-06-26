f489aebf1f9f048db50113eef96a39a839b2a2d7
KamilLelonek <squixy.sln@gmail.com>
2013-06-26 18:50:47 +0200

    Deleted Web test project

--- REST/src/com/example/rest/WebServerResponseListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.rest;
-
-public interface WebServerResponseListener {
-	// This method will be called after a query to web server has completed.
-	// If the query was successful webResponse is not null.
-	// If the query failed webResponse is null.
-	public void onWebServerResponse(WebResponse webResponse);
-}
