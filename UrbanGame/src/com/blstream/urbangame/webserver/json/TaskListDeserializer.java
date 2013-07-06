cc1a4558b4cf3f27862d38a3b7ec3815a1f6d850
jskudlarski <jakub.skudlarski@gmail.com>
2013-07-06 21:35:39 +0200

    Added Task list deserializing

--- UrbanGame/src/com/blstream/urbangame/webserver/json/TaskListDeserializer.java
+++ UrbanGame/src/com/blstream/urbangame/webserver/json/TaskListDeserializer.java
@@ -1,22 +1,36 @@
 package com.blstream.urbangame.webserver.json;
 
 import java.lang.reflect.Type;
+import java.util.LinkedList;
 
 import com.blstream.urbangame.database.entity.Task;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-public class TaskListDeserializer implements JsonDeserializer<Task> {
+public class TaskListDeserializer implements JsonDeserializer<TaskContainer> {
 	
 	@Override
-	public Task deserialize(final JsonElement json, final Type typeOfT, final JsonDeserializationContext context) {
+	public TaskContainer deserialize(final JsonElement json, final Type typeOfT,
+		final JsonDeserializationContext context) {
 		
 		JsonObject jsonObject = json.getAsJsonObject();
 		
-		//TODO - I need JSON example  to do this
+		JsonArray jsonTaskArray = jsonObject.get("_embedded").getAsJsonArray();
 		
-		return null;
+		LinkedList<Task> resultList = new LinkedList<Task>();
+		
+		for (int i = 0; i < jsonTaskArray.size(); i++) {
+			JsonObject jsonTask = jsonTaskArray.get(i).getAsJsonObject();
+			Task task = new Task() {}; //TODO I don't know what is the task type. I think it shouldn't be like this because we have to now what icon shoud be displayed
+			
+			task.setId(jsonTask.get("tid").getAsLong());
+			task.setTitle(jsonTask.get("name").getAsString());
+			resultList.add(task);
+		}
+		
+		return new TaskContainer(resultList);
 	}
 }
